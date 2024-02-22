ccc6f7bf4209770ac6bfa6aca0d39003d4bd684c
bernardslb14 <14joaobernardoleite@gmail.com>
2024-02-22 13:46:29 +0000

    data

---
+++ equipas.sql
@@ -0,0 +1,23 @@
+
+INSERT INTO public.equipa (id, derrotas, empates, imagem, nome, vitorias) VALUES (3, 0, 0, 'https://media.api-sports.io/football/teams/33.png', 'Manchester United', 0);
+INSERT INTO public.equipa (id, derrotas, empates, imagem, nome, vitorias) VALUES (4, 0, 0, 'https://media.api-sports.io/football/teams/34.png', 'Newcastle', 0);
+INSERT INTO public.equipa (id, derrotas, empates, imagem, nome, vitorias) VALUES (5, 0, 0, 'https://media.api-sports.io/football/teams/38.png', 'Watford', 0);
+INSERT INTO public.equipa (id, derrotas, empates, imagem, nome, vitorias) VALUES (6, 0, 0, 'https://media.api-sports.io/football/teams/39.png', 'Wolves', 0);
+INSERT INTO public.equipa (id, derrotas, empates, imagem, nome, vitorias) VALUES (7, 0, 0, 'https://media.api-sports.io/football/teams/40.png', 'Liverpool', 0);
+INSERT INTO public.equipa (id, derrotas, empates, imagem, nome, vitorias) VALUES (8, 0, 0, 'https://media.api-sports.io/football/teams/41.png', 'Southampton', 0);
+INSERT INTO public.equipa (id, derrotas, empates, imagem, nome, vitorias) VALUES (9, 0, 0, 'https://media.api-sports.io/football/teams/42.png', 'Arsenal', 0);
+INSERT INTO public.equipa (id, derrotas, empates, imagem, nome, vitorias) VALUES (10, 0, 0, 'https://media.api-sports.io/football/teams/44.png', 'Burnley', 0);
+INSERT INTO public.equipa (id, derrotas, empates, imagem, nome, vitorias) VALUES (11, 0, 0, 'https://media.api-sports.io/football/teams/45.png', 'Everton', 0);
+INSERT INTO public.equipa (id, derrotas, empates, imagem, nome, vitorias) VALUES (12, 0, 0, 'https://media.api-sports.io/football/teams/46.png', 'Leicester', 0);
+INSERT INTO public.equipa (id, derrotas, empates, imagem, nome, vitorias) VALUES (13, 0, 0, 'https://media.api-sports.io/football/teams/47.png', 'Tottenham', 0);
+INSERT INTO public.equipa (id, derrotas, empates, imagem, nome, vitorias) VALUES (14, 0, 0, 'https://media.api-sports.io/football/teams/48.png', 'West Ham', 0);
+INSERT INTO public.equipa (id, derrotas, empates, imagem, nome, vitorias) VALUES (15, 0, 0, 'https://media.api-sports.io/football/teams/49.png', 'Chelsea', 0);
+INSERT INTO public.equipa (id, derrotas, empates, imagem, nome, vitorias) VALUES (16, 0, 0, 'https://media.api-sports.io/football/teams/50.png', 'Manchester City', 0);
+INSERT INTO public.equipa (id, derrotas, empates, imagem, nome, vitorias) VALUES (17, 0, 0, 'https://media.api-sports.io/football/teams/51.png', 'Brighton', 0);
+INSERT INTO public.equipa (id, derrotas, empates, imagem, nome, vitorias) VALUES (18, 0, 0, 'https://media.api-sports.io/football/teams/52.png', 'Crystal Palace', 0);
+INSERT INTO public.equipa (id, derrotas, empates, imagem, nome, vitorias) VALUES (19, 0, 0, 'https://media.api-sports.io/football/teams/55.png', 'Brentford', 0);
+INSERT INTO public.equipa (id, derrotas, empates, imagem, nome, vitorias) VALUES (20, 0, 0, 'https://media.api-sports.io/football/teams/63.png', 'Leeds', 0);
+INSERT INTO public.equipa (id, derrotas, empates, imagem, nome, vitorias) VALUES (21, 0, 0, 'https://media.api-sports.io/football/teams/66.png', 'Aston Villa', 0);
+INSERT INTO public.equipa (id, derrotas, empates, imagem, nome, vitorias) VALUES (22, 0, 0, 'https://media.api-sports.io/football/teams/71.png', 'Norwich', 0);
+
+
